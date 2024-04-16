993b852e0ee31f793f469f335073233229264ff8
Hi im Andre <handrelukas@gmail.com>
2024-04-15 22:16:20 -0300

    cadastro de MIL

--- dados.py
+++ dados.py
@@ -27,3 +27,21 @@ if descricao == "S":
     print('Nome=',militar_cadastrado.idade)
     print('Nome=',militar_cadastrado.om)
     print('Nome=',militar_cadastrado.patente)
+
+nip = input("Deseja atribuir um nip ao militar?")
+if nip == 'sim':
+        
+    import random
+
+    x1 = str(random.randint(0,9))
+    x2 = str(random.randint(0,9))
+    x3 = str(random.randint(0,9))
+    x4 = str(random.randint(0,9))
+    x5 = str(random.randint(0,9))
+    x6 = str(random.randint(0,9))
+    x7 = str(random.randint(0,9))
+    x8 = str(random.randint(0,9))
+
+    nip = (x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8)
+
+    print(nip)
